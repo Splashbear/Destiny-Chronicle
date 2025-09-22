e6a2b89ad3c0f1669341c4d05c8c9c37306c66bf
Splashbear <knightcelestial@gmail.com>
2025-09-22 14:55:59 -0400

    fix: resolve final TypeScript compilation errors
    - Remove AppComponent from NgModule bootstrap array (it's standalone)
    - Remove AppComponent import from app.module.ts
    - All compilation errors now resolved, build successful
    - Web Worker implementation fully functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { TestApiComponent } from './components/test-api/test-api.component';
 import { BungieApiService } from './services/bungie-api.service';
 import { WorkerService } from './services/worker.service';
@@ -13,13 +12,11 @@ import { WorkerService } from './services/worker.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppComponent,
     TestApiComponent
   ],
   providers: [
     BungieApiService,
     WorkerService
-  ],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppModule { } 
